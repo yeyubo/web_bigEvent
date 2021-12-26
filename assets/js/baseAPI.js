34fd09fd020fb1b851f40bf7a8aec19b2f4ddca9
yeyubo <uboy_0205@163.com>
2021-12-26 10:33:34 +0800

    完成index主页

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,5 +1,24 @@
 // 每次调用$.get() | $.post | $.ajax时，会先调用ajaxPrefilter这个函数
 
 $.ajaxPrefilter((options) => {
-    console.log(options.url);
+    // console.log(options.url);
+    // options.url = options.url;
+
+    // 统一为有权限的接口设置headers请求头
+    if (options.url.indexOf("/my/") !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem("token") || "",
+        }
+    }
+    // 全局同意挂载complete函数
+    // 无论请求成功或者失败，都会执行complete函数
+    options.complete = (res) => {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            // 1.强制清空token
+            localStorage.removeItem("token");
+
+            // 2.强制跳转到登录页面
+            location.href = "/login.html";
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,54 @@
+$(() => {
+    const layer = layui.layer;
+    getUserInfo();
+
+    // 退出登录
+    $("#btnLogout").on("click", () => {
+        layer.confirm("确定退出登录吗？", { icon: 3, title: "退出登录" }, (confirm) => {
+            // 1.清除localStorage中保存的token
+            localStorage.removeItem("token");
+
+            // 2.重新跳转到登录页面
+            location.href = "/login.html"
+
+            // 3.关闭confirm询问框
+            layer.close(confirm);
+        })
+    })
+})
+
+// 请求用户信息
+function getUserInfo() {
+    $.ajax({
+        method: "GET",
+        url: "http://api-breakingnews-web.itheima.net/my/userinfo",
+        success: (res) => {
+            if (res.status !== 0) {
+                return layer.msg("获取用户信息失败");
+            }
+            renderAvatar(res.data);
+        },
+    })
+}
+
+// 渲染用户头像
+function renderAvatar(user) {
+    // 获取用户名称
+    let name = user.nickname || user.username;
+    $(".welcome").html("欢迎&nbsp;&nbsp;" + name);
+
+    // 按需渲染用户头像
+    if (user.user_pic !== null) {
+        // 渲染图片头像
+        $(".layui-nav-img")
+            .attr("src", user.user_pic)
+            .show();
+    } else {
+        // 渲染文本头像
+        $(".layui-nav-img").hide();
+        let first = name[0].toUpperCase();
+        $(".text-avatar").html(first).show();
+    }
+}
+
+
